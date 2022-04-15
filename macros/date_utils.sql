0078f8bf9d8b08a976f3e80b9602f729cbfc9e2a
Siarhei Nekhviadovich <Siarhei_Nekhviadovich@epam.com>
2022-04-15 16:07:48 +0200

    Added Date Utils for mocks

---
+++ macros/date_utils.sql
@@ -0,0 +1,11 @@
+{% macro n_days_ago(days=0) %}
+  {{ return (modules.datetime.datetime.now() - modules.datetime.timedelta(days)) }}
+{% endmacro %}
+
+{% macro to_epoch(dt) %}
+  {{ return ((dt - modules.datetime.datetime.utcfromtimestamp(0)).total_seconds() * 1000) }}
+{% endmacro %}
+
+{% macro to_iso(dt, sep=' ', timespec='milliseconds') %}
+  {{ return (dt.isoformat(sep, timespec)) }}
+{% endmacro %}
