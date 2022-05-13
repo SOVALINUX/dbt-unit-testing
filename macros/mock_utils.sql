c09e89d32d85491ba5359e0d94b3c33a295d8eb9
Siarhei Nekhviadovich <Siarhei_Nekhviadovich@epam.com>
2022-05-13 11:59:22 +0200

    Support wrapping csv values

--- macros/mock_utils.sql
+++ macros/mock_utils.sql
@@ -7,6 +7,8 @@
   {% set column_separator = options.get("column_separator", unit_tests_config.get("column_separator", ",")) %}
   {% set line_separator = options.get("line_separator", unit_tests_config.get("line_separator", "\n")) %}
   {% set type_separator = options.get("type_separator", unit_tests_config.get("type_separator", "::")) %}
+  {% set quote_symbol_to_wrap_values = options.get("quote_symbol_to_wrap_values", unit_tests_config.get("quote_symbol_to_wrap_values", "")) %}
+  {% set types_to_not_wrap = options.get("types_to_not_wrap", unit_tests_config.get("types_to_not_wrap", [])) %}
   {% set ns = namespace(col_names=[], col_types = [], col_values = [], row_values=[]) %}
 
   {% set rows = csv_table.split(line_separator) | map('trim') | reject('==', '') | list %}
@@ -23,9 +25,12 @@
     {% set cols = row.split(column_separator) | map('trim') | list %}
     {% set ns.col_values = [] %}
     {% for col in cols %}
-      {% set col_value = col %}
+      {% set col_value = quote_symbol_to_wrap_values ~ col ~ quote_symbol_to_wrap_values %}
       {% set col_type = ns.col_types[loop.index-1] %}
       {% if col_type is defined %}
+        {% if col_type in types_to_not_wrap %}
+          {% set col_value = col %}
+        {% endif %}
         {% set col_value = "CAST(" ~ col_value ~ " as " ~ col_type ~ ")" %}
       {% endif %}
       {% set col_value = col_value ~ " as " ~ ns.col_names[loop.index-1] %}
