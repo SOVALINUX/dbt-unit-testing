6352c260897559c86d70f94485c0d4545bfc401d
Siarhei Nekhviadovich <Siarhei_Nekhviadovich@epam.com>
2022-05-20 12:08:56 +0200

    Support work with date fields

--- macros/date_utils.sql
+++ macros/date_utils.sql
@@ -22,9 +22,11 @@
   {% set result = {} %}
   {% for ind in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 50, 100, 200, 300, 500, 1000] %}
     {% do result.update({'-' ~ ind ~ 'd_dt': dbt_unit_testing.n_days_ago(days=ind, now_posixtime=now_posixtime)}) %}
+    {% do result.update({'-' ~ ind ~ 'd_date': result['-' ~ ind ~ 'd_dt'].strftime('%Y-%m-%d')}) %}
     {% do result.update({'-' ~ ind ~ 'd_epoch': dbt_unit_testing.to_epoch(result['-' ~ ind ~ 'd_dt'])}) %}
     {% do result.update({'-' ~ ind ~ 'd_posix': dbt_unit_testing.to_posix(result['-' ~ ind ~ 'd_dt'])}) %}
     {% do result.update({'-' ~ ind ~ 'd_micros': (dbt_unit_testing.to_epoch(result['-' ~ ind ~ 'd_dt']) | int) * 1000}) %}
+    {% do result.update({'-' ~ ind ~ 'd_micros_from_date': (dbt_unit_testing.to_epoch(result['-' ~ ind ~ 'd_dt'].replace(hour=0, minute=0, second=0, microsecond=0)) | int) * 1000}) %}
   {% endfor %}
   {{ return ( result ) }}
 {% endmacro %}
